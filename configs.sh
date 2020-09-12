59f37b730399808001416724d88a6894b5ebc0f8
Evan Hirsh <contact@ehirsh.com>
2020-09-12 14:45:14 -0700

    change repo name and add installation instructions

---
+++ configs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/
+
+# install homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+
+brew install zsh
+
+# install antigen
+curl -L git.io/antigen > antigen.zsh
+
+# install zsh config
+curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.zshrc > .zshrc
+curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.p10k.zsh > .p10k.zsh
