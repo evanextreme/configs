1867d9989c64777576370b2eb8d66ed9fcad79eb
Evan Hirsh <evan@ehirsh.com>
2020-10-06 12:18:03 -0700

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -9,3 +9,6 @@ curl -L git.io/antigen > antigen.zsh
 # install zsh config
 curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.zshrc > .zshrc
 curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.p10k.zsh > .p10k.zsh
+
+# set up git lg alias
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
