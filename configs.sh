717cedfeea525269f187817a6e06dea9d06f3763
Evan Hirsh <evan@ehirsh.com>
2021-01-23 15:20:50 -0800

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -3,14 +3,19 @@
 # install homebrew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 
+echo 'eval \$(${HOMEBREW_PREFIX}/bin/brew shellenv)' >> ${shell_profile}
+eval \$(${HOMEBREW_PREFIX}/bin/brew shellenv)
+
+source ${shell_profile}
+
 brew install zsh
 
 # install antigen
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen > ~/antigen.zsh
 
 # install zsh config
-curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.zshrc > .zshrc
-curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.p10k.zsh > .p10k.zsh
+curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.zshrc > ~/.zshrc
+curl -L https://raw.githubusercontent.com/evanextreme/configs/master/.p10k.zsh > ~/.p10k.zsh
 
 # set up git lg alias
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
